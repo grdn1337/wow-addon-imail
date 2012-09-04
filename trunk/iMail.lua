5d4855f3c2c9bd25538ba43478bacd30d9acd0a7
grdn <grdn@bc3138d8-dc38-4e6c-8b2e-8a36bd578598>
2012-09-04 07:55:41 +0000

    - Fixed the integration of the iLib

--- trunk/iMail.lua
+++ trunk/iMail.lua
@@ -63,6 +63,8 @@ function iMail:Boot()
 	_G.MiniMapMailFrame:Hide();
 	
 	self:MailUpdate();
+	
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD");
 end
 iMail:RegisterEvent("PLAYER_ENTERING_WORLD", "Boot");
 
