81ccb888ea921b46fa88c8bce3b5cbab23a7b0ef
grdn <grdn@bc3138d8-dc38-4e6c-8b2e-8a36bd578598>
2013-09-14 07:48:08 +0000

    See r17

---
+++ 5.4-r1/Locales/enUS.lua
@@ -0,0 +1,9 @@
+local AddonName = ...;
+local L = LibStub("AceLocale-3.0"):NewLocale(AddonName, "enUS", true)
+
+--@localization(locale="enUS", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
+
+--@do-not-package@
+L["New Mails"] = true
+L["No Mails"] = true
+--@end-do-not-package@
